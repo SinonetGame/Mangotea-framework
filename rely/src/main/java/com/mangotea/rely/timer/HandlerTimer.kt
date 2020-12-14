900f5b82b7d638c6412953e86c38cd9bcee0ab00
dydytd3400 <87618865@qq.com>
2020-12-14 14:12:20 +0800

    优化HandlerTimer 增加重复次数控制

--- rely/src/main/java/com/mangotea/rely/timer/HandlerTimer.kt
+++ rely/src/main/java/com/mangotea/rely/timer/HandlerTimer.kt
@@ -1,29 +1,51 @@
 package com.mangotea.rely.timer
 
-import android.content.Context
 import android.os.Handler
 import android.os.Message
+import com.mangotea.rely.currentTimeMillis
+
 
 class HandlerTimer(
-        private val what: Int,
-        private val period: Long = 0,
-        private val delay: Long = 0,
-        blo: () -> Unit
+    /**
+     * unique code
+     */
+    private val what: Int,
+    /**
+     * 执行次数
+     * <0 暂不执行 0无限次
+     */
+    private var repeatCount: Int = 0,
+    /**
+     * 重复间隔时间
+     */
+    private val period: Long = 0,
+    /**
+     * 首次延迟时间
+     */
+    private val delay: Long = 0,
+    blo: () -> Unit
 ) : Handler() {
+    var executedCount = 0
+        private set
+
     private var todo: (() -> Unit)? = blo
     private var work = false
 
     override fun handleMessage(msg: Message) {
         if (msg.what == what && work) {
             todo?.let { it() }
-            runCatching { sendEmptyMessageDelayed(what, period) }
+            executedCount++
+            if (repeatCount == 0 || executedCount < repeatCount)
+                runCatching { sendEmptyMessageDelayed(what, period) }
         }
     }
 
     fun start() {
         if (!work) {
             work = true
-            runCatching { sendEmptyMessageDelayed(what, delay) }
+            if (repeatCount == 0 || executedCount < repeatCount) {
+                runCatching { sendEmptyMessageDelayed(what, delay) }
+            }
         }
     }
 
@@ -38,4 +60,17 @@ class HandlerTimer(
     }
 }
 
-fun Context.handlerTimer(period: Long = 0, delay: Long = 0, todo: () -> Unit) = HandlerTimer(hashCode(), period, delay, todo)
+fun handlerTimer(
+    period: Long = 0,
+    delay: Long = 0,
+    todo: () -> Unit
+) =
+    HandlerTimer("$currentTimeMillis-${Math.random()}".hashCode(), 0, period, delay, todo)
+
+fun handlerTimer(
+    repeatCount: Int,
+    period: Long = 0,
+    delay: Long = 0,
+    todo: () -> Unit
+) =
+    HandlerTimer("$currentTimeMillis-${Math.random()}".hashCode(), repeatCount, period, delay, todo)
